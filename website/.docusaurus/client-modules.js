8d3e246228a4f18ab148896b31b54f513b7659ff
Alex Zyukov <mhk_apel@outlook.com>
2026-05-06 15:43:07 +0600

    fix: stop tracking generated files

--- website/.docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("C:\\Users\\aleks\\Downloads\\edlm\\website\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\aleks\\Downloads\\edlm\\website\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\aleks\\Downloads\\edlm\\website\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Users\\aleks\\Downloads\\edlm\\website\\src\\css\\custom.css"),
-  require("C:\\Users\\aleks\\Downloads\\edlm\\website\\node_modules\\docusaurus-theme-redoc\\dist\\custom.css"),
-  require("C:\\Users\\aleks\\Downloads\\edlm\\website\\node_modules\\docusaurus-theme-redoc\\dist\\global.js"),
-];
